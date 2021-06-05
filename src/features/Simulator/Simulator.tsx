fd1f19a75adb2b315ddea8657b455a56c657317c
Lihong Han <littlefly14@gmail.com>
2021-06-05 01:00:41 -0500

    add animation

--- src/features/Simulator/Simulator.tsx
+++ src/features/Simulator/Simulator.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useSelector } from 'react-redux';
 import { getBeforeImg, getAfterImg} from './selectors';
+import { CSSTransition } from 'react-transition-group';
+import './simulator.css';
+import Tooltip from '@material-ui/core/Tooltip';
 
 export default function Simulator() {
   const beforeImg = useSelector(getBeforeImg)
   const afterImg = useSelector(getAfterImg)
+  const [showCurrentImg, setCurrentImg] = useState(true)
+  const [showTargetImg, setTargetImg] = useState(false)
  
   return (
-  <div style={{padding: "2%"}}>
-    <img src={ beforeImg } alt="current" />
-    <img src={ afterImg } alt="goal" />
+  <div style={{padding: "2%", display: 'flex', justifyContent: 'center'}}>
+    {showCurrentImg && (
+      <Tooltip title="Current Body Shape">
+        <img 
+          src={ beforeImg } 
+          alt="current" 
+          aria-label="current"
+          onClick={() => setTargetImg(true)}
+        />
+      </Tooltip>
+      
+    ) }
+    <CSSTransition
+        in={showTargetImg}
+        timeout={300}
+        classNames="alert"
+        unmountOnExit
+        onEnter={() => setCurrentImg(false)}
+        onExited={() => setCurrentImg(true)}
+      >
+      <Tooltip title="Target Body Shape">
+        <img 
+          src={ afterImg } 
+          alt="goal" 
+          aria-label="current"
+          onClick={() => setTargetImg(false)}
+        />
+      </Tooltip>
+      </CSSTransition>
+   
   </div>)
 };
